b6cc423eec368fca5a50e1219b9fdb4ebd5fc622
Icy-Joker <790238255@qq.com>
2024-03-12 10:49:11 +0800

    正则验证工具:添加身份证号格式验证正则表达式

--- src/CustomSDK/Tools/RegularExpressionValidator/private/RegularExpressionValidator.cpp
+++ src/CustomSDK/Tools/RegularExpressionValidator/private/RegularExpressionValidator.cpp
@@ -20,6 +20,7 @@ RegularExpressionValidator::RegularExpressionValidator(QWidget* parent, Qt::Wind
     ui->comboBox_MatchRules->addItem("Port", "(?:[0-5]?\\d{0,4})|(?:6[0-4]\\d{3})|(?:65[0-4]\\d{2})|(?:655[0-2]\\d)|(?:6553[0-5])");
     ui->comboBox_MatchRules->addItem("IP(v4)", "(?:(?:(?:25[0-5])|(?:2[0-4]\\d)|(?:[0-1]?\\d?\\d)).){3}(?:(?:25[0-5])|(?:2[0-4]\\d)|(?:[0-1]?\\d?\\d))");
     ui->comboBox_MatchRules->addItem("InternationAddress(v4)", "((?:(?:(?:25[0-5])|(?:2[0-4]\\d)|(?:[0-1]?\\d?\\d)).){3}(?:(?:25[0-5])|(?:2[0-4]\\d)|(?:[0-1]?\\d?\\d))):((?:[0-5]?\\d{0,4})|(?:6[0-4]\\d{3})|(?:65[0-4]\\d{2})|(?:655[0-2]\\d)|(?:6553[0-5]))");
+    ui->comboBox_MatchRules->addItem("IdentifyCardNumber","^([1-9]\\d{5})(\\d{4})((?:0[1-9])|(?:1[0-2]))((?:[0-2][1-9])|10|20|30|31)(\\d{2})(\\d)([Xx\\d])$");
   }
 }
 
