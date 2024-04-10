344bbb884e0de6366c107f68bf95031d1bfc5d4f
Icy-Joker <790238255@qq.com>
2024-04-10 17:50:59 +0800

    添加基础文件(夹)

---
+++ src/CustomSDK/arm-none-eabi.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+set(CMAKE_C_COMPILER "/opt/homebrew/opt/arm-none-eabi-gcc/bin/arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "/opt/homebrew/opt/arm-none-eabi-gcc/bin/arm-none-eabi-g++")
