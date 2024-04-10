f763b5bdb7bb3c923ee480c0dcab5ec040728ef5
Icy-Joker <790238255@qq.com>
2024-04-10 17:50:59 +0800

    添加基础文件(夹)

---
+++ src/CustomSDK/aarch64-elf.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_SYSROOT "/Users/icy-joker/Downloads/linux_aarch64")
+
+set(CMAKE_SYSTEM_NAME "linux")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+set(CMAKE_C_COMPILER "/opt/homebrew/opt/aarch64-elf-gcc/bin/aarch64-elf-gcc")
+set(CMAKE_CXX_COMPILER "/opt/homebrew/opt/aarch64-elf-gcc/bin/aarch64-elf-g++")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE  ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
---
+++ src/CustomSDK/x86_64-elf.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_SYSROOT "/Users/icy-joker/Downloads/x86_64-linux-gnu")
+
+set(CMAKE_SYSTEM_NAME "linux")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+set(CMAKE_C_COMPILER "/opt/homebrew/opt/x86_64-elf-gcc/bin/x86_64-elf-gcc")
+set(CMAKE_CXX_COMPILER "/opt/homebrew/opt/x86_64-elf-gcc/bin/x86_64-elf-g++")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
