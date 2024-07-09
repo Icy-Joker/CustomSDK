9fde0dbdf25b35651d0fa65b565db31c23fdc0ea
Icy-Joker <790238255@qq.com>
2024-07-09 14:04:07 +0800

    添加网络测试工具插件

---
+++ src/CustomSDK/Plugins/MagicTools/Plugin_NetworkTool/src/stdafx.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef __STDAFX_H__
+# define __STDAFX_H__
+
+#include "NetworkTool/NetworkTool_export.h"
+
+#include <BasicSupport/BasicSupport_export.h>
+#include <BasicFramework/BasicFramework_export.h>
+#include <BasicUIFramework/BasicUIFramework_export.h>
+#include <TcpSocketClient/TcpSocketClient_export.h>
+#include <TcpSocketServer/TcpSocketServer_export.h>
+#include <UdpSocketClient/UdpSocketClient_export.h>
+#include <UdpSocketServer/UdpSocketServer_export.h>
+
+#endif // !__STDAFX_H__
