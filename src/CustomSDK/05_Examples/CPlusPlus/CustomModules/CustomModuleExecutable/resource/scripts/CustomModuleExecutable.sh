9ef060b628d1a20496e3dbeb56682cd058d64cf6
Icy-Joker <790238255@qq.com>
2024-07-20 17:02:19 +0800

    添加自定义业务框架CPlusPlus测试程序

---
+++ src/CustomSDK/05_Examples/CPlusPlus/CustomModules/CustomModuleExecutable/resource/scripts/CustomModuleExecutable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#记录脚本所在位置
+export script_directory=`dirname $0`
+#进入脚本所在位置
+pushd "$script_directory" > /dev/null
+  pushd "../../" > /dev/null
+    export CustomSDK=$PWD
+    #export LD_LIBRARY_PATH=$CustomSDK/lib:$CustomSDK/test/plugins/CustomModule:$LD_LIBRARY_PATH
+    pushd "bin" > /dev/null
+      sh -c '../test/bin/CustomModuleExecutable -c "../test/config/config4CPlusPlus.xml"'
+    popd > /dev/null
+  popd > /dev/null
+popd > /dev/null
---
+++ src/CustomSDK/05_Examples/CPlusPlus/CustomModules/CustomModuleExecutable/src/CustomModuleExecutable.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef CUSTOMMODULEEXECUTABLE_H
+# define CUSTOMMODULEEXECUTABLE_H
+
+class CustomModuleExecutable
+{
+public:
+  explicit CustomModuleExecutable();
+  ~CustomModuleExecutable();
+public:
+  void doSomething();
+};
+
+#endif//!CUSTOMMODULEEXECUTABLE_H
