b8a675a071ff3536acc1b65a38da4339d47df7ee
Icy-Joker <790238255@qq.com>
2024-07-20 17:02:19 +0800

    添加自定义业务框架CPlusPlus测试程序

---
+++ src/CustomSDK/05_Examples/CPlusPlus/CustomModules/CustomModuleExecutable/src/CustomModuleExecutable.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef __CUSTOMMODULEEXECUTABLE_H__
+# define __CUSTOMMODULEEXECUTABLE_H__
+
+class CustomModuleExecutable
+{
+public:
+  explicit CustomModuleExecutable();
+  ~CustomModuleExecutable();
+public:
+  void doSomething();
+};
+
+#endif // !__CUSTOMMODULEEXECUTABLE_H__
