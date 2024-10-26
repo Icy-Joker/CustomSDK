7d6bcce280bf6d2df004b4075f163ab03afc6d3a
Icy-Joker <790238255@qq.com>
2024-10-26 23:19:58 +0800

    添加通用模块CPlusPlus模块示例

---
+++ src/CustomSDK/Examples/CPlusPlus/CustomModules/CustomModule/src/stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef __STDAFX_H__
+# define __STDAFX_H__
+
+#include "CustomModule/CustomModule_export.h"
+
+#include <BasicFramework/BasicFramework_export.h>
+
+#endif // !__STDAFX_H__
