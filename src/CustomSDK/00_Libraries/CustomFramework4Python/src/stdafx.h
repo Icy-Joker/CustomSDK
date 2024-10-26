0a8143a906380435e15de0d865bdf24b600b2c38
Icy-Joker <790238255@qq.com>
2024-10-27 00:30:49 +0800

    添加自定义业务框架Python包装

---
+++ src/CustomSDK/00_Libraries/CustomFramework4Python/src/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef __STDAFX_H__
+# define __STDAFX_H__
+
+#include "CustomFramework4Python/CustomFramework_export.h"
+
+#include <BasicSupport/BasicSupport_export.h>
+#include <BasicFramework/BasicFramework_export.h>
+
+#endif // !__STDAFX_H__
