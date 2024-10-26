499bf120d7ece938859d3985fb7afe0842f96b38
Icy-Joker <790238255@qq.com>
2024-10-26 23:19:58 +0800

    添加自定义业务框架CPlusPlus模块示例

---
+++ src/CustomSDK/05_Examples/CPlusPlus/CustomModules/CustomModule/src/CustomModule.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+
+#include "CustomModule/CustomModule.h"
+
+#include <iostream>
+
+CustomModule::CustomModule() :
+  AbstractCustomModule()
+{
+}
+
+CustomModule::~CustomModule()
+{
+}
+
+void CustomModule::doSayHello()
+{
+  std::cout << "[CPlusPlus]:CustomModule::Hello World!" << std::endl;
+}
+
+CUSTOMMODULE_C_WRAPPER(CustomModule);
