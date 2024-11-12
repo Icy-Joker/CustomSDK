ccc7bc729a63e637d5deace7853bbef1fbc72b8c
Icy-Joker <790238255@qq.com>
2024-11-12 10:14:41 +0800

    添加自定义业务框架Python模块示例

---
+++ src/CustomSDK/05_Examples/Python/CustomModules/CustomModule4Python/include/CustomModule4Python/CustomModule.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef CUSTOMMODULE_H
+# define CUSTOMMODULE_H
+
+#include <BasicFramework/AbstractCustomModule.h>
+
+extern "C"
+{
+  class CustomModule_API CustomModule : public AbstractCustomModule
+  {
+  public:
+    CustomModule();
+    ~CustomModule();
+  public:
+    void doSayHello() override;
+  };
+
+  //CUSTOMMODULE_C_WRAPPER(CustomModule);
+}
+
+#endif//!CUSTOMMODULE_H
---
+++ src/CustomSDK/05_Examples/Python/CustomModules/CustomModule4Python/src/stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef STDAFX_H
+# define STDAFX_H
+
+#include "CustomModule4Python/CustomModule_export.h"
+
+#include <BasicFramework/BasicFramework_export.h>
+
+#endif//!STDAFX_H
