d01a2d29d09346b6c242d4c6c63e0c416b209e74
Icy-Joker <790238255@qq.com>
2024-12-09 18:46:55 +0800

    更新SDK自述文件

--- src/CustomSDK/Plugins/MagicTools/Plugin_CustomDelegateDemo/src/ButtonDelegate.cpp
+++ src/CustomSDK/Plugins/MagicTools/Plugin_CustomDelegateDemo/src/ButtonDelegate.cpp
@@ -26,85 +26,65 @@ QWidget* ButtonDelegate::createEditor(QWidget* parent,const QStyleOptionViewItem
 
 void ButtonDelegate::paint(QPainter* painter,const QStyleOptionViewItem& option,const QModelIndex& index) const
 {
-  painter->save();
-
-  QStyleOptionButton tool_button_a_option;
-  tool_button_a_option.text = "test";
-  tool_button_a_option.direction = option.direction;
-  tool_button_a_option.rect = QRect(option.rect.left(),option.rect.top(),option.rect.width() / 2,option.rect.height());
-  tool_button_a_option.state = QStyle::State_Enabled;
-
-  QStyleOptionButton tool_button_b_option;
-  tool_button_b_option.text = "test2";
-  tool_button_b_option.direction = option.direction;
-  tool_button_b_option.rect = QRect(option.rect.left() + option.rect.width() / 2,option.rect.top(),option.rect.width() / 2,option.rect.height());
-  tool_button_b_option.state = QStyle::State_Enabled;
-
-  tool_button_a_option.state |= QStyle::State_Raised;
-  tool_button_b_option.state |= QStyle::State_Raised;
-
-  if(bool checked = index.data(Qt::ItemDataRole::CheckStateRole).value<Qt::CheckState>() == Qt::Checked)
   {
-    if(tool_button_a_option.rect.contains(option.widget->mapFromGlobal(QCursor::pos())))
-    {
-      qDebug() << "A";
-      tool_button_a_option.state |= QStyle::State_Sunken;
-    }
-    else if(tool_button_b_option.rect.contains(option.widget->mapFromGlobal(QCursor::pos())))
-    {
-      qDebug() << "B";
-      tool_button_b_option.state |= QStyle::State_Sunken;
-    }
-    else
-    {
-      qDebug() << "C";
-    }
-    qDebug() << option.widget->rect() << "|" << tool_button_a_option.rect << option.widget->mapFromGlobal(QCursor::pos());
+    QStyleOptionButton tool_button_a_option;
+    tool_button_a_option.text = "a";
+    tool_button_a_option.direction = option.direction;
+    tool_button_a_option.rect = QRect(option.rect.left(),option.rect.top(),option.rect.width() / 2,option.rect.height());
+    tool_button_a_option.state = QStyle::State_Enabled;
+
+    QPushButton push_button;
+    push_button.setStyleSheet("QPushButton{border-width: 0px;\
+                                position: absolute;\
+                                left: 0px;\
+                                top: 0px;\
+                                max-width: 80px;\
+                                width:80px;\
+                                height: 30px;\
+                                background: inherit;\
+                                background-color: rgba(255, 255, 255, 0);\
+                                border-width: 1px;\
+                                border-style: solid;\
+                                border-color: red;\
+                                border-radius: 10px;\
+                                font-size: 11px;\
+                                color: red;}\
+                                QPushButton:hover{background-color:red; color:#FFFFFF;}");
+    push_button.style()->drawControl(QStyle::CE_PushButton,&tool_button_a_option,painter,&push_button);
   }
-  else
   {
+    QStyleOptionButton tool_button_b_option;
+    tool_button_b_option.text = "b";
+    tool_button_b_option.direction = option.direction;
+    tool_button_b_option.rect = QRect(option.rect.left() + option.rect.width() / 2,option.rect.top(),option.rect.width() / 2,option.rect.height());
+    tool_button_b_option.state = QStyle::State_Enabled;
+
+    QPushButton push_button;
+    push_button.setStyleSheet("QPushButton{border-width: 0px;\
+                                position: absolute;\
+                                left: 0px;\
+                                top: 0px;\
+                                max-width:120px; \
+                                width:120px;\
+                                height: 30px;\
+                                background: inherit;\
+                                background-color: rgba(255, 255, 255, 0);\
+                                border-width: 1px;\
+                                border-style: solid;\
+                                border-color: rgba(2, 182, 212, 1);\
+                                border-radius: 10px;\
+                                font-size: 11px;\
+                                color: #000000;}\
+                                QPushButton:hover{background-color:rgba(2, 182, 212, 1); color:#FFFFFF;}");
+    push_button.style()->drawControl(QStyle::CE_PushButton,&tool_button_b_option,painter,&push_button);
   }
-
-  qobject_cast<QWidget*>(option.styleObject)->style()->drawPrimitive(QStyle::PE_PanelButtonCommand,&tool_button_a_option,painter,qobject_cast<QWidget*>(option.styleObject));
-  qobject_cast<QWidget*>(option.styleObject)->style()->drawPrimitive(QStyle::PE_FrameDefaultButton,&tool_button_a_option,painter,qobject_cast<QWidget*>(option.styleObject));
-  qobject_cast<QWidget*>(option.styleObject)->style()->drawControl(QStyle::CE_PushButton,&tool_button_a_option,painter,qobject_cast<QWidget*>(option.styleObject));
-
-  qobject_cast<QWidget*>(option.styleObject)->style()->drawPrimitive(QStyle::PE_PanelButtonCommand,&tool_button_b_option,painter,qobject_cast<QWidget*>(option.styleObject));
-  qobject_cast<QWidget*>(option.styleObject)->style()->drawPrimitive(QStyle::PE_FrameDefaultButton,&tool_button_b_option,painter,qobject_cast<QWidget*>(option.styleObject));
-  qobject_cast<QWidget*>(option.styleObject)->style()->drawControl(QStyle::CE_PushButton,&tool_button_b_option,painter,qobject_cast<QWidget*>(option.styleObject));
-
-  //qDebug() << qobject_cast<QWidget*>(option.styleObject)->style()->subElementRect(QStyle::SE_PushButtonLayoutItem,&option,qobject_cast<QWidget*>(option.styleObject));
-
-  painter->restore();
 }
 
 bool ButtonDelegate::editorEvent(QEvent* event,QAbstractItemModel* model,const QStyleOptionViewItem& option,const QModelIndex& index)
 {
   bool result = false;
 
-  if(QMouseEvent* mouse_event = dynamic_cast<QMouseEvent*>(event))
-  {
-    //model->blockSignals(true);
-    switch(mouse_event->type())
-    {
-      case QMouseEvent::MouseButtonPress:
-      {
-        model->setData(index,Qt::CheckState::Checked,Qt::ItemDataRole::CheckStateRole);
-        break;
-      }
-      case QMouseEvent::MouseButtonRelease:
-      {
-        model->setData(index,Qt::CheckState::Unchecked,Qt::ItemDataRole::CheckStateRole);
-        break;
-      }
-      default:
-      {
-        break;
-      }
-    }
-    //model->blockSignals(false);
-  }
-  //result = QStyledItemDelegate::editorEvent(event,model,option,index);
+  result = QStyledItemDelegate::editorEvent(event,model,option,index);
 
   return result;
 }
