c742ba1fc6a1048a34588af7fb884f8ce49f6644
Icy-Joker <790238255@qq.com>
2025-01-20 15:47:23 +0800

    添加自定义业务框架

---
+++ src/CustomSDK/Libraries/CustomFramework/src/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef __STDAFX_H__
+# define __STDAFX_H__
+
+#include "CustomFramework/CustomFramework_export.h"
+
+#include <BasicSupport/BasicSupport_export.h>
+#include <BasicFramework/BasicFramework_export.h>
+
+#endif // !__STDAFX_H__
