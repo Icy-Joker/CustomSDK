fe9cadc3dff983f1a0becb514f4c12e64216a141
Icy-Joker <790238255@qq.com>
2025-02-05 10:16:05 +0800

    添加微服务框架

---
+++ src/CustomSDK/00_Libraries/MicroserviceFramework/src/main.cpp
@@ -0,0 +1,90 @@
+#include "stdafx.h"
+
+#ifdef WIN32
+#include <windows.h>
+# define ATTRIBUTE_CONSTRUCTOR_DECLARE
+# define ATTRIBUTE_DESTRUCTOR_DECLARE
+#else
+#include <dlfcn.h>
+# define ATTRIBUTE_CONSTRUCTOR_DECLARE __attribute__((constructor))
+# define ATTRIBUTE_DESTRUCTOR_DECLARE __attribute__((destructor))
+#endif
+
+#include <cstdio>
+
+extern "C"
+{
+  MicroserviceFramework_API void ATTRIBUTE_CONSTRUCTOR_DECLARE initializeMicroserviceFramework()
+  {
+#ifdef WIN32
+#else
+    Dl_info dl_info;
+    if(dladdr((void*)initializeMicroserviceFramework,&dl_info))
+    {
+      //printf("Library:[%s] loaded\n",dl_info.dli_fname);
+    }
+#endif
+  }
+
+  MicroserviceFramework_API void ATTRIBUTE_DESTRUCTOR_DECLARE finalizeMicroserviceFramework()
+  {
+#ifdef WIN32
+#else
+    Dl_info dl_info;
+    if(dladdr((void*)finalizeMicroserviceFramework,&dl_info))
+    {
+      //printf("Library:[%s] unloaded\n",dl_info.dli_fname);
+    }
+#endif
+  }
+
+#ifdef WIN32
+  BOOL APIENTRY DllMain(HMODULE handle_module,DWORD reason_for_call,LPVOID flags_reserved)
+  {
+    bool resulut = true;
+
+    switch(reason_for_call)
+    {
+      case DLL_PROCESS_ATTACH:
+      {
+        char path[MAX_PATH];
+        if(GetModuleFileNameA(handle_module,path,MAX_PATH) != 0)///获取当前模块的全路径
+        {
+          //std::printf("Library:[%s] loaded\n",path);
+        }
+        else
+        {
+          MessageBoxA(NULL,"failed to get library path","Error",MB_OK);
+        }
+        break;
+      }
+      case DLL_THREAD_ATTACH:
+      {
+        break;
+      }
+      case DLL_THREAD_DETACH:
+      {
+        break;
+      }
+      case DLL_PROCESS_DETACH:
+      {
+        char path[MAX_PATH];
+        if(GetModuleFileNameA(handle_module,path,MAX_PATH) != 0)///获取当前模块的全路径
+        {
+          //printf("Library:[%s] unloaded\n",path);
+        }
+        else
+        {
+          MessageBoxA(NULL,"failed to get library path","Error",MB_OK);
+        }
+        break;
+      }
+      default:
+      {
+        break;
+      }
+    }
+    return true;
+  }
+#endif
+}
---
+++ src/CustomSDK/00_Libraries/MicroserviceFramework/src/stdafx.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef STDAFX_H
+# define STDAFX_H
+
+#include <BasicSupport/BasicSupport_export.h>
+#include "MicroserviceFramework/MicroserviceFramework_export.h"
+
+#endif//!__STDAFX_H__
