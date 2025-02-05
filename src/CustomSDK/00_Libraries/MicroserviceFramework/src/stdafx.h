9deb88ebb0ef4d71e3ee794b5cc5ccbbed4b49dd
Icy-Joker <790238255@qq.com>
2025-02-05 10:16:05 +0800

    添加微服务框架

---
+++ src/CustomSDK/00_Libraries/MicroserviceFramework/src/stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef STDAFX_H
+# define STDAFX_H
+
+#include "MicroserviceFramework/MicroserviceFramework_export.h"
+
+#include <BasicSupport/BasicSupport_export.h>
+
+#endif//!__STDAFX_H__
